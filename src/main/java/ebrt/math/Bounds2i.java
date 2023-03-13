974ef559c6d0e4822b84e21b219fba11ae4d1413
Ed Peters <epeters1111@gmail.com>
2023-03-13 09:10:19 -0400

    Adds iteration

--- src/main/java/ebrt/math/Bounds2i.java
+++ src/main/java/ebrt/math/Bounds2i.java
@@ -1,6 +1,8 @@
 package ebrt.math;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public record Bounds2i(Point2i min, Point2i max) implements Iterable<Point2i> {
 
@@ -30,13 +32,23 @@ public record Bounds2i(Point2i min, Point2i max) implements Iterable<Point2i> {
                 point.y() >= min.y() && point.y() < max.y());
     }
 
+    public Bounds2d toBounds2d() {
+        return new Bounds2d(min.toPoint2d(), max.toPoint2d());
+    }
+
     public Bounds2i intersect(Bounds2i other) {
-        throw new UnsupportedOperationException();
+        int minX = Math.max(min().x(), other.min.x());
+        int minY = Math.max(min().y(), other.min.y());
+        int maxX = Math.min(max().x(), other.max.x());
+        int maxY = Math.min(max().y(), other.max.y());
+        return new Bounds2i(new Point2i(minX, minY), new Point2i(maxX, maxY));
     }
 
     @Override
     public Iterator<Point2i> iterator() {
-        throw new UnsupportedOperationException();
+        List<Point2i> points = new ArrayList<>();
+        forEach((x,y) -> points.add(new Point2i(x,y)));
+        return points.iterator();
     }
 
     public void forEach(BoundsConsumer consumer) {
--- src/test/java/ebrt/math/TestBounds2i.java
+++ src/test/java/ebrt/math/TestBounds2i.java
@@ -1,2 +1,40 @@
-package ebrt.math;public class TestBounds2i {
+package ebrt.math;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestBounds2i {
+
+    @Test
+    public void testIntersectOverlap() {
+        Bounds2i b1 = new Bounds2i(new Point2i(-3, 4), new Point2i(5, -6));
+        Bounds2i b2 = new Bounds2i(new Point2i(-1, 2), new Point2i(3, -4));
+        assertEquals(b1.intersect(b2), b2);
+        assertEquals(b2.intersect(b1), b2);
+    }
+
+    @Test
+    public void testIntersectNonOverlap() {
+        Bounds2i b1 = new Bounds2i(new Point2i(-3, 4), new Point2i(5, -6));
+        Bounds2i b2 = new Bounds2i(new Point2i(-7, 2), new Point2i(9, -4));
+        Bounds2i b3 = new Bounds2i(new Point2i(-3, 2), new Point2i(5, -4));
+        assertEquals(b1.intersect(b2), b3);
+        assertEquals(b2.intersect(b1), b3);
+    }
+
+    @Test
+    public void testIterator() {
+
+        Bounds2i b = new Bounds2i(new Point2i(-1, 1), new Point2i(1, -2));
+
+        List<Point2i> p = new ArrayList<>();
+        b.iterator().forEachRemaining(p::add);
+
+        System.err.println(p);
+        assertEquals(12, p.size());
+    }
 }
